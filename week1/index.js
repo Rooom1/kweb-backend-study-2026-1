5098900d039ea0832f754b2334363661c29f33c7
Rooom1 <bangjun0930@gmail.com>
2026-04-08 10:30:45 +0000

    week1 2022320019 방원준 과제 제출입니다

--- week1/index.js
+++ week1/index.js
@@ -3,7 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get("/food",(req, res) =>{ 
-    res.send("<h1>Hello,World!</h1>");
+    res.send("<h1>Hi human!</h1>");
+});
+
+app.get("/burger",(req, res) =>{ 
+    res.send("<h1>Im Hungry!</h1>");
 });
 
 app.listen(8080, () => {
