7f4a5668a2fb96a5c7281631526a6a689b5a78c7
Rooom1 <bangjun0930@gmail.com>
2026-04-08 09:58:07 +0000

    week 1 exercise

--- week1/index.js
+++ week1/index.js
@@ -1,1 +1,11 @@
 const express = require("express");
+
+const app = express();
+
+app.get("/food",(req, res) =>{ 
+    res.send("<h1>Hello,World!</h1>");
+});
+
+app.listen(8080, () => {
+    console.log("Server listening on port 3000!");
+});
